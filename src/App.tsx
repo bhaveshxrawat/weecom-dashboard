daabb2f6ddc01a4c369c03052d97a9d66f52f459
Bhavesh Rawat <rawatbhavesh2001@gmail.com>
2025-08-30 22:07:56 +0530

    setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import DashboardHeader from "@/components/DashboardHeader";
+
+function App() {
+	return (
+		<div className="grid bg-gray-50 min-h-[100svh] grid-rows-[auto_1fr]">
+			<DashboardHeader />
+		</div>
+	);
+}
+
+export default App;
