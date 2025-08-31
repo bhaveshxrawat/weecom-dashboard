9258bc6d08fb352e47a7ba51adfbf5a92c25c752
Bhavesh Rawat <rawatbhavesh2001@gmail.com>
2025-08-31 18:52:00 +0530

    sidebar

---
+++ src/components/DashboardSidebar.tsx
@@ -0,0 +1,56 @@
+import { Home, Package, Settings, ShoppingBag, Users } from "lucide-react";
+import { cn } from "@/lib/utils";
+
+const items = [
+	{
+		title: "Home",
+		url: "#",
+		icon: Home,
+	},
+	{
+		title: "Products",
+		url: "#",
+		icon: Package,
+	},
+	{
+		title: "Orders",
+		url: "#",
+		icon: ShoppingBag,
+	},
+	{
+		title: "Customers",
+		url: "#",
+		icon: Users,
+	},
+	{
+		title: "Settings",
+		url: "#",
+		icon: Settings,
+	},
+] as const;
+
+function DashboardSidebar() {
+	return (
+		<aside className="bg-white p-4 rounded-2xl drop-shadow-sm">
+			<nav>
+				<ul>
+					{items.map((item) => (
+						<li key={item.title} className="last:mt-auto">
+							<a
+								href={item.url}
+								className={cn(
+									"flex gap-2.5 items-center px-3 py-4 rounded-xl ",
+									item.title === "Products" && "bg-accent",
+								)}
+							>
+								<item.icon size={18} />
+								{item.title}
+							</a>
+						</li>
+					))}
+				</ul>
+			</nav>
+		</aside>
+	);
+}
+export default DashboardSidebar;
