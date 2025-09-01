4c14e4ff1bbea1ee9ab2825fbc0e25192a365388
Bhavesh Rawat <rawatbhavesh2001@gmail.com>
2025-09-02 01:20:35 +0530

    dolor

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,17 +4,3 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
-
-// biome-ignore lint/suspicious/noExplicitAny: <needed>
-export function debounce<T, U extends any[]>(
-	fn: (...args: U) => T,
-	delay = 300,
-) {
-	let timer: ReturnType<typeof setTimeout>;
-	return (...args: U) => {
-		clearTimeout(timer);
-		timer = setTimeout(() => {
-			fn(...args);
-		}, delay);
-	};
-}
--- src/services/apiProducts.ts
+++ src/services/apiProducts.ts
@@ -13,6 +13,8 @@ type ProductsResponse = {
 	skip: number;
 	limit: number;
 };
+
+type newProductType = Omit<Products, "id">;
 export async function getProducts(skip = 0, search = "") {
 	const fetchURL = new URL(
 		`/products?limit=${PRODUCTS_PER_PAGE}&skip=${skip}&select=title,price,category,stock`,
@@ -31,5 +33,23 @@ export async function getProducts(skip = 0, search = "") {
 		return data as ProductsResponse;
 	} catch {
 		throw new Error("Something went wrong while fetching the products.");
+	}
+}
+
+export async function addProduct(data: newProductType) {
+	const fetchURL = new URL(`/products/add`, API_BASEURL);
+	try {
+		const fetchRes = await fetch(fetchURL, {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(data),
+		});
+		if (!fetchRes.ok) {
+			throw new Error("Encountered an issue while adding a new product.");
+		}
+		const fetchData = await fetchRes.json();
+		return fetchData as Products;
+	} catch {
+		throw new Error("Something went wrong while adding a product.");
 	}
 }
